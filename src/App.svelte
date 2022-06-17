8d97049ac0e85205000bd027ed44f745a55b992e
ali navidi <ali_navidi24@yahoo.com>
2022-06-17 18:53:34 +0430

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { onMount } from "svelte";
+  import Card from "./components/Card.svelte";
+
+  let results = [];
+
+  onMount(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "25a916eea1mshc663c709757d1f9p19f877jsnada2ece8fc1b",
+        "X-RapidAPI-Host": "rawg-video-games-database.p.rapidapi.com",
+      },
+    };
+
+    fetch(
+      "https://rawg-video-games-database.p.rapidapi.com/games?key=2e13e80be7204f9ea9d6ea08bc058a68",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        results = response.results;
+        console.log(results);
+      })
+      .catch((err) => console.error(err));
+  });
+</script>
+
+<main>
+  {#if results[0]}
+    <Card games={results} />
+  {/if}
+</main>
---
+++ src/components/Card.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let games = [];
+</script>
+
+<div class="games">
+  {#each games as game (game.id)}
+    <div class="card">
+      <img src={game.background_image} alt="" />
+      <p>{game.name}</p>
+      <p class="rating">{game.rating}</p>
+    </div>
+  {/each}
+</div>
